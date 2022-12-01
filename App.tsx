c2dc0a325aee9509d8f51cfc9ce3c6ebca8f39ad
IgorVeludo2 <velvet.guimaraes@gmail.com>
2022-12-01 19:05:48 -0300

    Loading component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter'
+import { 
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo'
+
+import { ThemeProvider } from 'styled-components';
+
 import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
+import theme from './src/styles';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  });
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Home /> : <Loading />}
+    </ThemeProvider>
   );
 }
 
